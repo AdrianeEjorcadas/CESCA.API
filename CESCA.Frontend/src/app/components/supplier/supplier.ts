194bded73842908883f5516d9977b22e0b07e67a
Adriane E. <adrianeejorcadas97@gmail.com>
2025-10-27 13:23:03 +0800

    10/27/2025 - filter fields

--- CESCA.Frontend/src/app/components/supplier/supplier.ts
+++ CESCA.Frontend/src/app/components/supplier/supplier.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
 import { SupplierSearchParameter } from '../../models/search-parameter';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-supplier',
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule, MatCheckboxModule],
   templateUrl: './supplier.html',
   styleUrl: './supplier.css'
 })
 export class Supplier {
+
+  searchParams : SupplierSearchParameter = {
+    pageNumber: 1,
+    pageSize: 10,
+    searchTerm: '',
+    isArchived: false
+  };
+
+  search(){
+    console.log(this.searchParams);
+  }
   
 }
